0ceecbb2cf4d6f30283a47824d0273e9a0427f1d
Mike Mooney <mmooney@mmdbsolutions.com>
2014-12-22 11:26:47 -0500

    setting up project

--- Sriracha.Server/Program.cs
+++ Sriracha.Server/Program.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
+using Topshelf.ServiceConfigurators;
 
 namespace Sriracha.Server
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            var exitCode = HostFactory.Run(host =>
+            {
+                host.Service<SrirachaApplication>(service =>
+                {
+                    service.ConstructUsing(() => new SrirachaApplication());
+                    service.WhenStarted(a => a.Start());
+                    service.WhenStopped(a => a.Stop());
+                });
+
+                host.SetDescription("An application to manage sending sms messages and provide message status");
+                host.SetDisplayName("Sriracha");
+                host.SetServiceName("Sriracha");
+                host.RunAsNetworkService();
+            });
+            return (int)exitCode;
         }
     }
 }
---
+++ Sriracha.Server/SrirachaApplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sriracha.Server
+{
+    public class SrirachaApplication
+    {
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}

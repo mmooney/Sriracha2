704d05f3d4e3dd24a7b20d500164382caa0e3773
Mike Mooney <mmooney@mmdbsolutions.com>
2015-02-04 21:12:37 -0500

    Dropkick web deployment work in progress

---
+++ Sriracha.DeployTask.WebApplication/Dropkick/DropkickWebDeployment.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dropkick.Configuration.Dsl;
+using dropkick.Configuration.Dsl.Files;
+using dropkick.Configuration.Dsl.Authentication;
+using dropkick.Configuration.Dsl.Iis;
+using MMDB.Shared;
+
+
+namespace Sriracha.DeployTask.WebApplication.Dropkick
+{
+    internal class DropkickWebDeployment : Deployment<DropkickWebDeployment, DropkickWebDeploymentSettings>
+    {
+        public static Role Website { get; set; }
+
+        public DropkickWebDeployment()
+        {
+            Define(settings =>
+            {
+                DeploymentStepsFor(Website,
+                                    s =>
+                                    {
+                                        if (!string.IsNullOrEmpty(settings.TargetMachineUserName) && !string.IsNullOrEmpty(settings.TargetMachinePassword))
+                                        {
+                                            s.WithAuthentication(settings.TargetMachineUserName, settings.TargetMachinePassword);
+
+                                            s.OpenFolderShareWithAuthentication(@"{{TargetWebsitePath}}", settings.TargetMachineUserName, settings.TargetMachinePassword);
+                                        }
+
+                                        string appPoolName = settings.ApplicationPoolName;
+                                        if (string.IsNullOrWhiteSpace(appPoolName))
+                                        {
+                                            appPoolName = settings.SiteName;
+                                        }
+                                        var iis = s.Iis7Site(settings.SiteName, @"{{TargetWebsitePath}}", default(int))
+                                            .VirtualDirectory(StringHelper.IsNullOrEmpty(settings.VirtualDirectoryName, "/"))
+                                            .SetAppPoolTo(appPoolName, pool =>
+                                            {
+                                                pool.SetRuntimeToV4();
+                                                //pool.UseClassicPipeline();
+                                                //pool.Enable32BitAppOnWin64();
+                                            }).SetPathTo(@"{{TargetWebsitePath}}");
+                                    });
+            });
+        }
+
+        public Dictionary<string, string> GetDefaultServerMap()
+        {
+            var returnValue = new Dictionary<string, string>();
+            returnValue.Add("Website", string.Empty);
+            return returnValue;
+        }
+    }
+}

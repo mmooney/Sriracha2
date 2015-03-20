b950b40b44dc78b5181abc607814f4eb83fa4a68
Mike Mooney <mmooney@mmdbsolutions.com>
2015-03-19 21:03:57 -0400

    nuget build

---
+++ NugetPackage.bat
@@ -0,0 +1,2 @@
+SET BUILD_NUMBER=2.0.0.0
+.\ThirdParty\nant-0.92\NAnt.exe -buildfile:.\Sriracha2.build NuGetPackageRunners
--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
+SET BUILD_NUMBER=2.0.0.0
 .\ThirdParty\nant-0.92\NAnt.exe -buildfile:.\Sriracha2.build BuildSolution PackageSolution

ec8c41cc1faae79a34c0c3cec47fc225ddd212d0
Mike Mooney <mmooney@mmdbsolutions.com>
2015-04-23 17:09:19 -0400

    Starting to build command line publisher

---
+++ Sriracha.Publisher/Options.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sriracha.Publisher
+{
+    public class Options
+    {
+        public Options()
+        {
+            this.PublishVerb = new PublishSubOptions();
+        }
+
+        [VerbOption("publish")]
+        public PublishSubOptions PublishVerb { get; set; }
+
+        public class PublishSubOptions
+        {
+            [Option('x', "url")]
+            public string Url { get; set; }
+
+            [Option('u', "user")]
+            public string UserName { get; set; }
+
+            [Option('u', "password")]
+            public string Password { get; set; }
+        }
+    }
+}
---
+++ Sriracha.Publisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace Sriracha.Publisher
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            string invokedVerb;
+            object invokedVerbInstance;
+            bool result = CommandLine.Parser.Default.ParseArguments(args, options,
+                (verb, subOptions) =>
+                {
+                    invokedVerb = verb;
+                    invokedVerbInstance = subOptions;
+                });
+            if (!result)
+            {
+                // Values are available here
+                Console.WriteLine("No good, " + CommandLine.Text.HelpText.AutoBuild(options));
+            }
+            else 
+            {
+                Console.WriteLine("good");
+            }
+            Console.ReadKey();
+        }
+    }
+}
